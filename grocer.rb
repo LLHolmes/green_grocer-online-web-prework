90931abb36ed444dab6ce57ead4fb9b340e97cda
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-12 17:13:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,60 @@
 def consolidate_cart(cart)
-  # code here
+  cart_hash = {}
+  cart.each do |item|
+    item.each do |food, data|
+      item[food][:count] = cart.count(item)
+      if cart_hash.has_key?(food) == false
+        cart_hash[food] = data
+      end
+    end
+  end
+  cart_hash
 end
+
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |deal|
+    if cart.has_key?(deal[:item])
+      if cart[deal[:item]][:count] >= deal[:num]
+        cart[deal[:item]][:count] = cart[deal[:item]][:count] -   deal[:num]
+        if cart.has_key?("#{deal[:item]} W/COUPON")
+          cart["#{deal[:item]} W/COUPON"][:count] += 1
+        else
+          cart["#{deal[:item]} W/COUPON"] = {:price => deal[:cost], :clearance => cart[deal[:item]][:clearance], :count => 1}
+        end
+      end
+    end
+  end
+  cart
 end
+
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |food, data|
+    if data[:clearance] == true
+      data[:price] = (data[:price] * 0.8).round(1)
+    end
+  end
 end
 
+
 def checkout(cart, coupons)
-  # code here
+  total = 0
+  puts coupons
+  puts cart
+  consolidate_cart(cart)
+  puts cart
+  apply_coupons(cart, coupons)
+  puts cart
+  apply_clearance(cart)
+  puts cart
+  cart.each do |food, data|
+    total = total + (data[:price] * data[:count])
+  end
+  puts total
+  puts cart
+  if total > 100
+    total = (total * 0.9).round(1)
+  end
+  total
 end
